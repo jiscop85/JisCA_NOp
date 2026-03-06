1d2f3992ff484764c20a3f0c481df89b0c6e4901
jiscop85 <127202191+jiscop85@users.noreply.github.com>
2026-03-07 00:36:03 +0330

    Add files via upload

---
+++ frontend/src/UI/carousel.jsx
@@ -0,0 +1,18 @@
+import * as React from "react"
+import useEmblaCarousel from "embla-carousel-react";
+import { ArrowLeft, ArrowRight } from "lucide-react"
+
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+
+const CarouselContext = React.createContext(null)
+
+function useCarousel() {
+  const context = React.useContext(CarouselContext)
+
+  if (!context) {
+    throw new Error("useCarousel must be used within a <Carousel />")
+  }
+
+  return context
+}
