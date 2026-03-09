9b8791d4032a7e0b4b1404e93bb17b7898f5d52a
jiscop85 <127202191+jiscop85@users.noreply.github.com>
2026-03-09 09:03:02 +0330

    Update collapsible.jsx

--- frontend/src/UI/collapsible.jsx
+++ frontend/src/UI/collapsible.jsx
@@ -1,1 +1,9 @@
 import * as CollapsiblePrimtive from "@radix-ui/react-collapsible"
+
+const Collapsible = CollapsiblePrimitive.Root
+
+const CollapsibleTrigger = CollapsiblePrimitive.CollapsibleTrigger
+
+const CollapsibleContent = CollapsiblePrimitive.CollapsibleContent
+
+export { Collapsible, CollapsibleTrigger, CollapsibleContent }
