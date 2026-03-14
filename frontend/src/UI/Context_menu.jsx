48a579d314fd75fb64f24a8c3ee9bdd5303f9b52
jiscop85 <127202191+jiscop85@users.noreply.github.com>
2026-03-14 17:20:49 -0400

    Update Context_menu.jsx

--- frontend/src/UI/Context_menu.jsx
+++ frontend/src/UI/Context_menu.jsx
@@ -1,2 +1,15 @@
 Import * as React from "react"
 Import * as ContextMenuPrimitive from 
+"@radix-ui/react-context-menu"
+Import { Check, chevronRight, Circle } from "lucid-react"
+
+Import { cn } from "@/lib/utils"
+
+const contextMenu = ContextMenuPrimitive.Root
+
+const contextMenuTrigger = ContextMenuPrimitive.Trigger
+
+
+const contextMenuGroup = ContextMenuPrimitive.Group
+
+
