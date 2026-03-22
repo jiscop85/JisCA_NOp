f803e6e1ba1270e6c31c41f1d20a86e8c63dd718
jiscop85 <127202191+jiscop85@users.noreply.github.com>
2026-03-22 17:34:01 -0400

    Update crud.py

--- backend/crud.py
+++ backend/crud.py
@@ -3,4 +3,10 @@ from models import Task
 
 def get_tasks(db: Session, search: str = ""):
     return db.query(Task).filter(Task.title.contains(search)).all()
+def create_task(db: Session, task):
+    new_task = Task(title=task.title, status=task.status)
+    db.add(new_task)
+    db.commit()
+    db.refresh(new_task)
+    return new_task
 
